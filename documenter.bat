9f4cebbed7ee375984bab6ad0870bb9c4f1a1fe9
LinnartSF <linnart.felkl@hotmail.com>
2023-01-04 20:31:26 +0100

    .

---
+++ documenter.bat
@@ -0,0 +1,7 @@
+python -m pydoc -w animator
+python -m pydoc -w framework
+python -m pydoc -w modelling
+python -m pydoc -w ui
+python -m pydoc -w test
+python -m pydoc -w strategies
+mv *.html docs
